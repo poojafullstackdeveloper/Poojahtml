3b07bf82f94b9999c6b45c1246d8704568912a51
poojafullstackdeveloper <156500266+poojafullstackdeveloper@users.noreply.github.com>
2024-03-12 23:11:42 +0530

    Add files via upload

---
+++ kids.js
@@ -0,0 +1,43 @@
+const carousel = document.querySelector(".carousel");
+
+let isDown = false;
+
+let startX;
+
+let scrollLeft;
+
+carousel.addEventListener("mousedown", (e) => {
+
+    isDown = true;
+
+    startX = e.pageX - carousel.offsetLeft;
+
+    scrollLeft = carousel.scrollLeft;
+
+});
+
+carousel.addEventListener("mouseleave", () => {
+
+    isDown = false;
+
+});
+
+carousel.addEventListener("mouseup", () => {
+
+    isDown = false;
+
+});
+
+carousel.addEventListener("mousemove", (e) => {
+
+    if (!isDown) return;
+
+    e.preventDefault();
+
+    const x = e.pageX - carousel.offsetLeft;
+
+    const walk = x - startX;
+
+    carousel.scrollLeft = scrollLeft - walk;
+
+});
